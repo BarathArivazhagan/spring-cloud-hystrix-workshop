2f0a33bc3eee86cc7c1cb9ae7ef13f2f614aadba
Barath Arivazhagan <barathmacdec91@gmail.com>
2018-08-14 00:40:44 +0530

    code change

--- config-instances-turbine/hystrix-client-service/src/main/java/com/barath/app/HystrixClientServiceApplication.java
+++ config-instances-turbine/hystrix-client-service/src/main/java/com/barath/app/HystrixClientServiceApplication.java
@@ -3,12 +3,10 @@ package com.barath.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableScheduling
 public class HystrixClientServiceApplication {
 
 	public static void main(String[] args) {
--- config-instances-turbine/hystrix-client-service/src/main/java/com/barath/app/controller/CustomerController.java
+++ config-instances-turbine/hystrix-client-service/src/main/java/com/barath/app/controller/CustomerController.java
@@ -3,14 +3,19 @@ package com.barath.app.controller;
 import com.barath.app.model.Customer;
 import com.barath.app.service.CustomerService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 @RestController
 public class CustomerController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private final CustomerService customerService;
 	
 
@@ -24,7 +29,8 @@ public class CustomerController {
 	@GetMapping("/customers")
     @HystrixCommand(commandKey = "getCustomers")
     public List<Customer> customers(){
-
+		
+		if(logger.isInfoEnabled()) logger.info("customers invoked");
         return customerService.getCustomers();
 
     }
@@ -32,7 +38,8 @@ public class CustomerController {
 	@GetMapping("/customersWithThreadPool20")
     @HystrixCommand(commandKey = "getCustomersWithThreadPool20",threadPoolKey= "thread20")
     public List<Customer> customersWithThreadPool20(){
-
+		
+		if(logger.isInfoEnabled()) logger.info("customersWithThreadPool20 invoked");
         return customerService.getCustomers();
 
     }
@@ -40,7 +47,8 @@ public class CustomerController {
 	@GetMapping("/customersWithThreadPool30")
     @HystrixCommand(commandKey = "getCustomersWithThreadPool",threadPoolKey= "thread30")
     public List<Customer> customersWithThreadPool30(){
-
+		
+		if(logger.isInfoEnabled()) logger.info("customersWithThreadPool30 invoked");
         return customerService.getCustomers();
 
     }
@@ -48,7 +56,8 @@ public class CustomerController {
 	@GetMapping("/customersWithThreadPool40")
     @HystrixCommand(commandKey = "getCustomersWithThreadPool40",threadPoolKey= "thread40")
     public List<Customer> customersWithThreadPool40(){
-
+		
+		if(logger.isInfoEnabled()) logger.info("customersWithThreadPool40 invoked");
         return customerService.getCustomers();
 
     }
@@ -56,7 +65,8 @@ public class CustomerController {
 	@GetMapping("/customersWithThreadPool50")
     @HystrixCommand(commandKey = "getCustomersWithThreadPool50",threadPoolKey= "thread50")
     public List<Customer> customersWithThreadPool(){
-
+		
+		if(logger.isInfoEnabled()) logger.info("customersWithThreadPool50 invoked");
         return customerService.getCustomers();
 
     }
